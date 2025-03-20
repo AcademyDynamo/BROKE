23dfc8fa224d43f49b0eb19363c3c6e221f9582c
AcademyDynamo <artist7064@gmail.com>
2025-03-21 04:31:38 +0700

    Update app.js

--- app.js
+++ app.js
@@ -3,3 +3,15 @@ document.querySelectorAll(".shirt").forEach((shirt, index) => {
         alert(Вы выбрали игрока ${index + 1});
     });
 });
+
+document.getElementById("btn1").addEventListener("click", () => {
+    alert("Нажата кнопка 1");
+});
+
+document.getElementById("btn2").addEventListener("click", () => {
+    alert("Нажата кнопка 2");
+});
+
+document.getElementById("btn3").addEventListener("click", () => {
+    alert("Нажата кнопка 3");
+});
