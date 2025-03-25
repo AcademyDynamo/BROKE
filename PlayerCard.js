9cf35cfec631c47f80ebf8410177511be268d21f
AcademyDynamo <artist7064@gmail.com>
2025-03-25 12:56:43 +0700

    Update PlayerCard.js

--- PlayerCard.js
+++ PlayerCard.js
@@ -1,1 +1,14 @@
+import React from 'react';
+import CaptainIcon from './CaptainIcon';
 
+const PlayerCard = ({ player, selectPlayer, toggleCaptain }) => {
+  return (
+    <div className="player-card" onClick={() => selectPlayer(player)}>
+      <img src="вставь изображение 2" alt="player" className="player-image" />
+      <span className="name">{player.name}</span>
+      <CaptainIcon active={player.captain} onClick={() => toggleCaptain(player.id)} />
+    </div>
+  );
+};
+
+export default PlayerCard;
