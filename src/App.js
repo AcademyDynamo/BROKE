668b59a136e66fbfd26e8ac44c5d06fc538e0dd0
AcademyDynamo <artist7064@gmail.com>
2025-03-25 15:19:54 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,12 @@
+import React from "react";
+import Field from "./components/Field";
 
+const App = () => {
+  return (
+    <div className="app">
+      <Field />
+    </div>
+  );
+};
+
+export default App;
