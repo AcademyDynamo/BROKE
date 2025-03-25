174ccb20f6ffd31ac146d4bb25b21617f5a6ef02
AcademyDynamo <artist7064@gmail.com>
2025-03-25 13:27:00 +0700

    Update Pitch.js

--- Pitch.js
+++ Pitch.js
@@ -3,35 +3,28 @@ import PlayerCard from "./PlayerCard";
 import CaptainIcon from "./CaptainIcon";
 
 const initialPlayers = [
-    { id: 1, name: "Игрок 1", position: { top: "100px", left: "50px" } },
-    { id: 2, name: "Игрок 2", position: { top: "200px", left: "200px" } },
-    { id: 3, name: "Игрок 52", position: { top: "200px", left: "50px" } },
-    { id: 4, name: "Игрок 42", position: { top: "400px", left: "80px" } },
-    { id: 5, name: "Игрок 23", position: { top: "400px", left: "30px" } },
-    { id: 6, name: "Игрок 22", position: { top: "700px", left: "100px" } },
-    { id: 7, name: "Игрок 21", position: { top: "700px", left: "20px" } },
+  { id: 1, name: "Player 1", position: "Forward" },
+  { id: 2, name: "Player 2", position: "Midfielder" },
+  { id: 3, name: "Player 3", position: "Defender" },
 ];
 
-const Pitch = () => {
-    const [players, setPlayers] = useState(initialPlayers);
-    const [captain, setCaptain] = useState(null);
+function Pitch({ captain, setCaptain }) {
+  const [players, setPlayers] = useState(initialPlayers);
 
-    const handleSetCaptain = (id) => {
-        setCaptain(id);
-    };
+  const handleCaptainSelect = (id) => {
+    setCaptain(id);
+  };
 
-    return (
-        <div className="pitch">
-            {players.map((player) => (
-                <PlayerCard
-                    key={player.id}
-                    player={player}
-                    isCaptain={captain === player.id}
-                    onSetCaptain={() => handleSetCaptain(player.id)}
-                />
-            ))}
+  return (
+    <div className="pitch">
+      {players.map((player) => (
+        <div key={player.id} className="player-container">
+          <PlayerCard player={player} />
+          <CaptainIcon isActive={captain === player.id} onClick={() => handleCaptainSelect(player.id)} />
         </div>
-    );
-};
+      ))}
+    </div>
+  );
+}
 
 export default Pitch;
