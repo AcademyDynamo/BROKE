c332d2d91efc3096f92a2adaeada529d882bdb91
AcademyDynamo <artist7064@gmail.com>
2025-03-25 16:34:37 +0700

    Update database.js

--- database.js
+++ database.js
@@ -1,51 +1,36 @@
-// Database with players and teams
 const footballers = [
-    {
-        id: 1,
-        name: "Player 1",
-        team: "Team A",
-        position: "Forward",
-        teamLogo: "images/Ресурс 1.svg",
-        image: "images/Group 5.svg"
-    },
-    {
-        id: 2,
-        name: "Player 2",
-        team: "Team B",
-        position: "Midfielder",
-        teamLogo: "images/Ресурс 1.svg",
-        image: "images/Group 5.svg"
-    },
-    {
-        id: 3,
-        name: "Player 3",
-        team: "Team C",
-        position: "Defender",
-        teamLogo: "images/Ресурс 1.svg",
-        image: "images/Group 5.svg"
-    }
-    {
-        id: 5,
-        name: "Player 5",
-        team: "Team C",
-        position: "Defender",
-        teamLogo: "images/Ресурс 1.svg",
-        image: "images/Group 5.svg"
-    }
-    {
-        id: 4,
-        name: "Player 3",
-        team: "Team B",
-        position: "Defender",
-        teamLogo: "images/Ресурс 1.svg",
-        image: "images/Group 5.svg"
-    }
-    {
-        id: 6,
-        name: "Player 6",
-        team: "Team B",
-        position: "Defender",
-        teamLogo: "images/Ресурс 1.svg",
-        image: "images/Group 5.svg"
-    }
+  {
+    id: 1,
+    name: "Player 1",
+    teamImage: "images/team1.svg",
+    position: "Forward",
+    teamInfo: "Team 1: Champions League contenders.",
+    playerImage: "images/Ресурс 1.svg"
+  },
+  {
+    id: 2,
+    name: "Player 2",
+    teamImage: "images/team2.svg",
+    position: "Midfielder",
+    teamInfo: "Team 2: Top of the league.",
+    playerImage: "images/Ресурс 1.svg"
+  },
+  {
+    id: 3,
+    name: "Player 3",
+    teamImage: "images/team3.svg",
+    position: "Defender",
+    teamInfo: "Team 3: Strong defensive lineup.",
+    playerImage: "images/Ресурс 1.svg"
+  },
+  {
+    id: 4,
+    name: "Player 4",
+    teamImage: "images/team4.svg",
+    position: "Goalkeeper",
+    teamInfo: "Team 4: Excellent goalkeeping skills.",
+    playerImage: "images/Ресурс 1.svg"
+  }
 ];
+
+export default footballers;
