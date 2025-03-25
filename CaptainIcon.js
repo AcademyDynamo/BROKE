7a9e5b9b5a37a9dacde49c8ab9d41a6de1aaec06
AcademyDynamo <artist7064@gmail.com>
2025-03-25 12:55:28 +0700

    Create CaptainIcon.js

---
+++ CaptainIcon.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CaptainIcon = ({ active, onClick }) => {
+  return (
+    <div className={captain-icon ${active ? 'active' : 'inactive'}} onClick={onClick}>
+      C
+    </div>
+  );
+};
+
+export default CaptainIcon;
