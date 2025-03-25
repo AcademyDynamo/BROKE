7056099d2eee5fb2b24e96c20f2b7d108cc25502
AcademyDynamo <artist7064@gmail.com>
2025-03-25 16:21:57 +0700

    Update database.js

--- database.js
+++ database.js
@@ -5,47 +5,47 @@ const footballers = [
         name: "Player 1",
         team: "Team A",
         position: "Forward",
-        teamLogo: "images/teamA-logo.svg",
-        image: "images/Ресурс 1.svg"
+        teamLogo: "images/Ресурс 1.svg",
+        image: "images/Group 5.svg"
     },
     {
         id: 2,
         name: "Player 2",
         team: "Team B",
         position: "Midfielder",
-        teamLogo: "images/teamB-logo.svg",
-        image: "images/Ресурс 1.svg"
+        teamLogo: "images/Ресурс 1.svg",
+        image: "images/Group 5.svg"
     },
     {
         id: 3,
         name: "Player 3",
         team: "Team C",
         position: "Defender",
-        teamLogo: "images/teamC-logo.svg",
-        image: "images/Ресурс 1.svg"
+        teamLogo: "images/Ресурс 1.svg",
+        image: "images/Group 5.svg"
     }
     {
         id: 5,
         name: "Player 5",
         team: "Team C",
         position: "Defender",
-        teamLogo: "images/teamC-logo.svg",
-        image: "images/Ресурс 1.svg"
+        teamLogo: "images/Ресурс 1.svg",
+        image: "images/Group 5.svg"
     }
     {
         id: 4,
         name: "Player 3",
         team: "Team B",
         position: "Defender",
-        teamLogo: "images/teamC-logo.svg",
-        image: "images/Ресурс 1.svg"
+        teamLogo: "images/Ресурс 1.svg",
+        image: "images/Group 5.svg"
     }
     {
         id: 6,
         name: "Player 6",
         team: "Team B",
         position: "Defender",
-        teamLogo: "images/teamC-logo.svg",
-        image: "images/Ресурс 1.svg"
+        teamLogo: "images/Ресурс 1.svg",
+        image: "images/Group 5.svg"
     }
 ];
