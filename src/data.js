74fa662bc5a580ce0acb3cc831a53d4c2aeae636
AcademyDynamo <artist7064@gmail.com>
2025-03-25 15:11:04 +0700

    Update data.js

--- src/data.js
+++ src/data.js
@@ -1,1 +1,46 @@
+const footballers = [
+  {
+    id: 1,
+    name: "Лионель Месси",
+    position: "Нападающий",
+    team: "Интер Майами",
+    teamImage: "вставь изображение n"
+  },
+  {
+    id: 2,
+    name: "Килиан Мбаппе",
+    position: "Нападающий",
+    team: "ПСЖ",
+    teamImage: "вставь изображение n"
+  },
+  {
+    id: 3,
+    name: "Кевин Де Брюйне",
+    position: "Полузащитник",
+    team: "Манчестер Сити",
+    teamImage: "вставь изображение n"
+  }
+  {
+    id: 4,
+    name: "Кевин Де Брюйне",
+    position: "Полузащитник",
+    team: "Алтай",
+    teamImage: "вставь изображение n"
+  }
+  {
+    id: 5,
+    name: "Кевин Де Брюйне",
+    position: "Полузащитник",
+    team: "Манчестер Сити",
+    teamImage: "вставь изображение n"
+  }
+  {
+    id: 6,
+    name: "Кевин Де Брюйне",
+    position: "Полузащитник",
+    team: "Алтай",
+    teamImage: "вставь изображение n"
+  }  
+];
 
+export default footballers;
