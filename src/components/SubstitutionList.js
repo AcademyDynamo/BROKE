e85f844fa9c3d1f0c5870e9d36a069f4b67201db
AcademyDynamo <artist7064@gmail.com>
2025-03-25 15:18:52 +0700

    Update SubstitutionList.js

--- src/components/SubstitutionList.js
+++ src/components/SubstitutionList.js
@@ -1,1 +1,19 @@
+import React from "react";
+import footballers from "../data";
 
+const SubstitutionList = ({ selectedPlayer, onSubstitute }) => {
+  return (
+    <div className="substitution-list">
+      <h3>Выберите замену для {selectedPlayer.name}:</h3>
+      {footballers.map(player => (
+        player.id !== selectedPlayer.id && (
+          <div key={player.id} onClick={() => onSubstitute(player)} className="substitution-option">
+            {player.name} ({player.team})
+          </div>
+        )
+      ))}
+    </div>
+  );
+};
+
+export default SubstitutionList;
