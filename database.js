78f9e16fb7e0f5c127bcf14a9a835a9a887bb32c
AcademyDynamo <artist7064@gmail.com>
2025-03-25 16:13:48 +0700

    Update database.js

--- database.js
+++ database.js
@@ -24,6 +24,30 @@ const footballers = [
         teamLogo: "images/teamC-logo.svg",
         image: "images/Ресурс 1.svg"
     }
+    {
+        id: 5,
+        name: "Player 5",
+        team: "Team C",
+        position: "Defender",
+        teamLogo: "images/teamC-logo.svg",
+        image: "images/Ресурс 1.svg"
+    }
+    {
+        id: 4,
+        name: "Player 3",
+        team: "Team B",
+        position: "Defender",
+        teamLogo: "images/teamC-logo.svg",
+        image: "images/Ресурс 1.svg"
+    }
+    {
+        id: 6,
+        name: "Player 6",
+        team: "Team B",
+        position: "Defender",
+        teamLogo: "images/teamC-logo.svg",
+        image: "images/Ресурс 1.svg"
+    }
 ];
 
 // Function to handle player click animation
