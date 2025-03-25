a3ad669ce1beef5b0c850f2a91fd07fdb5e3ba4e
AcademyDynamo <artist7064@gmail.com>
2025-03-25 15:17:30 +0700

    Update Field.js

--- src/components/Field.js
+++ src/components/Field.js
@@ -1,49 +1,35 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import PlayerIcon from "./PlayerIcon";
 import CaptainIcon from "./CaptainIcon";
-import "./styles.css";
+import SubstitutionList from "./SubstitutionList";
+import footballers from "../data";
 
 const Field = () => {
-  const [players, setPlayers] = useState([]);
+  const [players, setPlayers] = useState(footballers);
   const [selectedPlayer, setSelectedPlayer] = useState(null);
   const [captain, setCaptain] = useState(null);
 
-  useEffect(() => {
-    fetch("http://localhost:5000/players")
-      .then((res) => res.json())
-      .then((data) => {
-        setPlayers(data);
-        const captainPlayer = data.find((p) => p.isCaptain === 1);
-        setCaptain(captainPlayer);
-      });
-  }, []);
-
-  const handlePlayerClick = (player) => {
+  const handleSelectPlayer = (player) => {
     setSelectedPlayer(player);
   };
 
-  const handleSetCaptain = (player) => {
-    fetch("http://localhost:5000/set-captain", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ playerId: player.id }),
-    }).then(() => {
-      setCaptain(player);
-    });
+  const handleSubstitute = (newPlayer) => {
+    setPlayers(players.map(p => (p.id === selectedPlayer.id ? newPlayer : p)));
+    setSelectedPlayer(null);
   };
 
   return (
     <div className="field">
-      <img src="images/pitch.svg" alt="Football Field" className="field-img" />
-      {players.map((player) => (
-        <div key={player.id} className="player-slot">
-          <PlayerIcon player={player} onClick={handlePlayerClick} />
-          <CaptainIcon
-            isActive={captain?.id === player.id}
-            onClick={() => handleSetCaptain(player)}
-          />
-        </div>
+      <img src="images/pitch 1.svg" alt="Поле" className="field-image" />
+      {players.map(player => (
+        <PlayerIcon 
+          key={player.id} 
+          player={player} 
+          onClick={() => handleSelectPlayer(player)} 
+        />
       ))}
+      <CaptainIcon players={players} captain={captain} setCaptain={setCaptain} />
+      {selectedPlayer && <SubstitutionList selectedPlayer={selectedPlayer} onSubstitute={handleSubstitute} />}
     </div>
   );
 };
