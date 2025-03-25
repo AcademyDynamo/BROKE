5505c904bf4f052d08c6436cc314cdad99a6a0b9
AcademyDynamo <artist7064@gmail.com>
2025-03-25 13:26:47 +0700

    Update App.js

--- App.js
+++ App.js
@@ -2,13 +2,15 @@ import React, { useState } from "react";
 import Pitch from "./components/Pitch";
 import "./styles.css";
 
-const App = () => {
-    return (
-        <div className="app">
-            <h1 className="header">Fantasy League</h1>
-            <Pitch />
-        </div>
-    );
-};
+function App() {
+  const [captain, setCaptain] = useState(null);
+
+  return (
+    <div className="app">
+      <h1 className="header">Fantasy Mini App</h1>
+      <Pitch captain={captain} setCaptain={setCaptain} />
+    </div>
+  );
+}
 
 export default App;
