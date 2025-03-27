7ee4cec4e37fa32ebc32de246a424e5da8942fde
AcademyDynamo <artist7064@gmail.com>
2025-03-27 17:32:00 +0700

    Update PlayerCard.js

--- src/components/PlayerCard.js
+++ src/components/PlayerCard.js
@@ -1,12 +1,13 @@
-export default function PlayerCard({ player }) {
-    const playerImage = player.photo 
-        ? `./public/images/players/${player.photo}` 
+export default function PlayerCard({ player, isSelected }) {
+    const playerImage = player.photo
+        ? ./public/images/players/${player.photo}
         : "./public/images/Group 5.svg";
 
-    return `
+    return 
         <div class="player-card">
             <img src="${playerImage}" alt="${player.name}" class="player-img">
             <span class="player-name">${player.name}</span>
+            <span class="player-status">${isSelected ? "Выбран" : "+/выбери игрока"}</span>
         </div>
-    `;
+    ;
 }
