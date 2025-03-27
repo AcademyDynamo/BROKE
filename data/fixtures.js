64cc86ec883919769bbc2cf679b3a081006a2f93
AcademyDynamo <artist7064@gmail.com>
2025-03-27 15:22:29 +0700

    Create fixtures.js

---
+++ data/fixtures.js
@@ -0,0 +1,17 @@
+const fixtures = [
+  {
+    homeTeam: 'Ливерпуль',
+    awayTeam: 'Манчестер Сити',
+    date: '2023-10-29',
+    time: '18:30',
+  },
+  {
+    homeTeam: 'Арсенал',
+    awayTeam: 'Тоттенхэм',
+    date: '2023-11-05',
+    time: '16:00',
+  },
+  // ... другие матчи
+];
+
+export default fixtures;
