c65462c9e2074b3b4f7e268273f152cfb5bc2c77
AcademyDynamo <artist7064@gmail.com>
2025-03-27 15:23:04 +0700

    Create teams.js

---
+++ data/teams.js
@@ -0,0 +1,17 @@
+const teams = [
+  {
+    name: 'Ливерпуль',
+    stadium: 'Энфилд',
+    city: 'Ливерпуль',
+    image: 'liverpool.png',
+  },
+  {
+    name: 'Манчестер Сити',
+    stadium: 'Этихад',
+    city: 'Манчестер',
+    image: 'mancity.png',
+  },
+  // ... другие команды
+];
+
+export default teams;
