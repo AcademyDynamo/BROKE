547767c7f9e71cef3472cea98c240b94a4bab3ac
AcademyDynamo <artist7064@gmail.com>
2025-03-27 13:03:53 +0700

    Create PlayerList.js

---
+++ components/PlayerList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PlayerCard from './PlayerCard';
+
+function PlayerList({ players }) {
+  return (
+    <div className="player-list">
+      {players.map(player => (
+        <PlayerCard key={player.id} player={player} />
+      ))}
+    </div>
+  );
+}
+
+export default PlayerList;
